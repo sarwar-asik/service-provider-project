d95d18fc961a19d566d15465c7580e67439b8044
sarwar-asik <shasik2021@gmail.com>
2022-11-08 13:00:00 +0600

    service cards added

--- src/pages/services/ServiceCard.js
+++ src/pages/services/ServiceCard.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-const ServiceCard = () => {
+const ServiceCard = ({service}) => {
+    const {_id,name,img,rating,price ,detail,review}= service;
+
     return (
-        <div>
-            
+        <div className="col-12 col-md-4 col-lg-4 my-2">
+        <div class="card">
+          <img src={img} className='mx-auto rounded ' class="card-img-top" alt="..." style={{'width':'100%','height':'200px'}} />
+          <div class="card-body">
+            <h5 class="card-title">{name}</h5>
+            <p class="card-text">
+             {detail}
+            </p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
+  return (
+    <div className="my-3 mx-auto" style={{ maxWidth: "90%" }}>
+      <h1>Services {services.length}</h1>
+      <div class="card-group row ">
+        {services.map((service) => {
+          return (
+            <ServiceCard key={service._id} service={service}></ServiceCard>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
